1492c1769bf413e2fd5cd1087d0a11b80f84b258
karasuv <karasuv@inbox.ru>
2021-03-29 20:28:25 +0300

    домашняя работа 5

--- src/main/java/homework5.java
+++ src/main/java/homework5.java
@@ -1,14 +1,13 @@
 
 public class homework5 {
     public static void main(String[] args) {
-        Employee emp = new Employee("Петров Е Е", "директор", "mail@mail.ru", "556644", 30000, 66);
-        System.out.println(emp.toString());
+
         Employee[] empArray = new Employee[5];
-        empArray[0] = new Employee("Иванов", "сотрудник 1", "emp1@mail.ru", "1111111", 10000, 21);
-        empArray[1] = new Employee("Петров", "сотрудник 2", "emp2@mail.ru", "2222222", 20000, 32);
-        empArray[2] = new Employee("Сидоров", "сотрудник 3", "emp3@mail.ru", "3333333", 30000, 43);
-        empArray[3] = new Employee("Абрамович", "сотрудник 4", "emp4@mail.ru", "4444444", 40000, 54);
-        empArray[4] = new Employee("Казаков", "сотрудник 5", "emp5@mail.ru", "5555555", 50000, 35);
+        empArray[0] = new Employee("Иванов", "грузчик", "emp1@mail.ru", "1111111", 10000, 21);
+        empArray[1] = new Employee("Петров", "дизайнер", "emp2@mail.ru", "2222222", 20000, 32);
+        empArray[2] = new Employee("Сидоров", "логист", "emp3@mail.ru", "3333333", 30000, 43);
+        empArray[3] = new Employee("Абрамович", "главный бухгалтер", "emp4@mail.ru", "4444444", 40000, 54);
+        empArray[4] = new Employee("Казаков", "директор", "emp5@mail.ru", "5555555", 50000, 35);
 
         System.out.println("все сотрудники:");
         for (int i=0;i< empArray.length;i++){
